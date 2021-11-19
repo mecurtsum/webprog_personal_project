ad34111fbfc5cf56c0a772d914b16877296a669a
unknown <derbedeneva@proitr.ru>
2021-11-19 20:31:23 +0300

    кнопка на всех страничках

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
+if (document.querySelector('.btn1') !== null) {
+    const switcher1 = document.querySelector('.btn1');
 
-switcher.addEventListener('click', function() {
-    var theme = document.getElementById('theme');
-    if (theme.href.includes("style.css")) {
-        theme.href = "style3.css";
-    } else {
-        theme.href = "style.css";
-    }
-});
+    switcher1.addEventListener('click', function() {
+        var theme = document.getElementById('theme');
+        if (theme.href.includes("style.css")) {
+            theme.href = "style3.css";
+        } else {
+            theme.href = "style.css";
+        }
+    })
+};
+if (document.querySelector('.btn2') !== null) {
+    const switcher2 = document.querySelector('.btn2');
+
+    switcher2.addEventListener('click', function() {
+        var theme = document.getElementById('theme');
+        if (theme.href.includes("style2.css")) {
+            theme.href = "style4.css";
+        } else {
+            theme.href = "style2.css";
+        }
+    })
+};
