ce79a3788cd5cfb683e63c82c3e4f3e042bf55d9
unknown <derbedeneva@proitr.ru>
2021-11-19 23:15:36 +0300

    подключила апи

--- api.js
+++
@@ -1,9 +0,0 @@
-fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/301', {
-    method: 'GET',
-    headers: {
-        'X-API-KEY': 'a4432d19-3639-4366-b26a-afe6ff1424eb',
-        'Content-Type': 'application/json',
-    },
-})
-.then(res => res.json())
-.then(json => console.log(json))
